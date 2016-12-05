b4c19605e231c4399e37d72493b6e8825a4ae180
lucaskeven <lucaskevencosta@gmail.com>
2016-12-05 21:52:34 -0200

    Funcionalidades da tela de login.

--- SCDWeb/src/main/java/br/com/SCDWeb/model/usuario/Usuario.java
+++ SCDWeb/src/main/java/br/com/SCDWeb/model/usuario/Usuario.java
@@ -6,9 +6,21 @@ package br.com.SCDWeb.model.usuario;
 import java.io.Serializable;
 
 /**
- * @author user
+ * @author Lucas Keven C. de Souza
  *
  */
 public class Usuario implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private final String USUARIO = "admin";
+	private final String SENHA = "admin";
+	
+	public String getUSUARIO() {
+		return USUARIO;
+	}
+	
+	public String getSENHA() {
+		return SENHA;
+	}
 
 }
--- SCDWeb/src/main/java/br/com/SCDWeb/view/UsuarioBean.java
+++ SCDWeb/src/main/java/br/com/SCDWeb/view/UsuarioBean.java
@@ -5,10 +5,57 @@ package br.com.SCDWeb.view;
 
 import java.io.Serializable;
 
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+import br.com.SCDWeb.model.usuario.UsuarioRN;
+
 /**
  * @author user
  *
  */
+
+@SessionScoped
+@ManagedBean(name = "viewUsuario")
 public class UsuarioBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private String usuarioEntrada;
+	private String senhaEntrada;
+	private String error;
+
+	public String actionLogar() {
+		if (new UsuarioRN().validaLogin(usuarioEntrada, senhaEntrada))
+			return "listagem_equipamento.xhtml";
+		else {
+			// Cria uma mensagem
+			FacesMessage msg = new FacesMessage("Usuário ou senha inválido!");
+			// Obtém a instância atual do FacesContext e adiciona a mensagem de erro nele
+			FacesContext.getCurrentInstance().addMessage("erro", msg);
+			return null;
+		}
+	}
+
+	public String getUsuarioEntrada() {
+		return usuarioEntrada;
+	}
+
+	public void setUsuarioEntrada(String usuarioEntrada) {
+		this.usuarioEntrada = usuarioEntrada;
+	}
+
+	public String getSenhaEntrada() {
+		return senhaEntrada;
+	}
+
+	public void setSenhaEntrada(String senhaEntrada) {
+		this.senhaEntrada = senhaEntrada;
+	}
+
+	public String getError() {
+		return error;
+	}
 
 }
