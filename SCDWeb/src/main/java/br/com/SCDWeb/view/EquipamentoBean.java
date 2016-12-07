d41002cdc8e2b7cfaba6649858b8eeaa008362bd
Leonardo1922 <leogs0919>
2016-12-07 09:59:54 -0200

    Criação do Bean Equipamento

---
+++ SCDWeb/src/main/java/br/com/SCDWeb/view/EquipamentoBean.java
@@ -0,0 +1,49 @@
+package br.com.SCDWeb.view;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+
+import br.com.SCDWeb.model.equipamento.Equipamento;
+import br.com.SCDWeb.model.equipamento.EquipamentoRN;
+/**
+ * 
+ * @author Leonardo
+ *
+ */
+
+@ManagedBean (name = "viewEquipamento")
+public class EquipamentoBean implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+
+	private Equipamento equipamento = new Equipamento();
+	
+	public String actionApagar(){
+		new EquipamentoRN().apagar(equipamento);
+		return "listagem_equipamento?faces-redirect=true";
+	}
+
+	public List<Equipamento> getTodosEquipamentos(){
+		return new EquipamentoRN().listarTodos();
+	}
+	
+	public String actionSalvar(){
+		new EquipamentoRN().salvar(equipamento);
+		return "listagem_equipamento?faces-redirect=true";
+	}
+	
+	public String actionNovo(){
+		this.equipamento = new Equipamento();
+		return "cadastro_equipamento";
+	}
+	
+	public Equipamento getEquipamento() {
+		return equipamento;
+	}
+
+	public void setEquipamento(Equipamento equipamento) {
+		this.equipamento = equipamento;
+	}
+}
